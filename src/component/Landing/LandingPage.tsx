a5bb7dcaeb61b10555e834ed18042a3279236f41
amirhosseinsadegh <mohmmadjalali44@gmail.com>
2025-11-10 08:13:38 -0800

    token handeler in handel  ip

--- src/component/Landing/LandingPage.tsx
+++ src/component/Landing/LandingPage.tsx
@@ -1,25 +1,43 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Header } from "../Navbar/Header";
 import { Section } from "./section/responsive/section";
-import toast, { Toaster } from "react-hot-toast";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { Icon } from "@mui/material";
 
-const LandingPage: React.FC = () => {
-  localStorage.setItem("Token", "salam");
+interface userobject {
+  Token: any;
+  DarckMode: boolean;
+  ip: any;
+  Password: any;
+}
+const LandingPage: React.FC<userobject> = () => {
+  const ClientHandeler = async () => {
+    const GetipSystem = await axios.get("https:api.ipify.org/?format=json");
 
-  const HandelToken = () => {
-    const ValedatToke = localStorage.getItem("Token");
-    if (!ValedatToke) {
-      toast.error("ابتدا ثبت نام کنید");
+    const saveToken = localStorage.setItem("Tokenuser", "");
+    const DarckModevalu = localStorage.setItem("Darckmode", "false");
+    const SaveIp = localStorage.setItem("ip", `${GetipSystem.data.ip}`);
+    const InPassword = localStorage.setItem("Pass", "");
+    const langueg =localStorage.setItem("lan","fa")
+
+    const GetToken = localStorage.getItem("Tokenuser");
+    const GetDarckModevalu = localStorage.getItem("Darckmode");
+    const GetIp = localStorage.getItem("ip");
+    const GetInPassword = localStorage.getItem("Pass");
+
+    if (GetToken === "") {
+      toast.error("ابتدا ثبت نام کنید",{style:{fontFamily:"Peyda, sans-serif",}});
     } else {
-      toast.success("خوش آمدید", { position: "bottom-right" });
+      toast("به جابسراخوش آمدید",{position:'bottom-right',style:{backgroundColor:`#1E3A8A`,fontFamily:"Peyda, sans-serif",color:`#fff`}})
     }
   };
-  useEffect(() => {
-    HandelToken();
-  }, []);
+
+  ClientHandeler();
 
   return (
     <>
+      
       <Header />
       <Section />
     </>
