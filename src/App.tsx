df4203c5263c7ff06023d4df83791d16aaf45976
amirhosseinsadegh <mohmmadjalali44@gmail.com>
2025-12-29 21:46:23 -0800

    Job list Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {  useEffect } from "react";
+import React, { useEffect } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { LandingPage } from "./component/Landing/LandingPage";
 import "./index.css";
@@ -6,24 +6,29 @@ import "./App.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import toast, { Toaster } from "react-hot-toast";
-import { SigenUp} from "./component/Sighenup/Sighenup"
+import { SigenUp } from "./component/Sighenup/Sighenup";
+import { Joblist } from "./component/Job list/Job list";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    Component: LandingPage   ,
+    Component: LandingPage,
   },
   {
-    path:"/Sighenup",
+    path: "/Sighenup",
     Component: SigenUp,
-  }
+  },
+  {
+    path: "/Joblist",
+    Component: Joblist,
+  },
 ]);
 
 const App: React.FC = () => {
   useEffect(() => {
     AOS.init({
       duration: 1000,
-      once: true, 
+      once: true,
     });
   }, []);
   return (
